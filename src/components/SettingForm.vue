627d29b2065a2855322e9ca2c006bef0dcca3551
ilesoviy <ilisovij861@gmail.com>
2023-08-08 00:02:13 +0900

    Got recent blockchash

--- src/components/SettingForm.vue
+++ src/components/SettingForm.vue
@@ -2,6 +2,8 @@
 import Button from './reusable/Button.vue';
 import FormInput from './reusable/FormInput.vue';
 
+import * as web3 from '@solana/web3.js';
+
 export default {
 	components: {
 		Button,
@@ -14,11 +16,22 @@ export default {
 			recentBlockhash: '',
 			nodes: [
 				{
-					url: 'https://solana-api.projectserum.com'
-				}, {
-					url: 'https://api.mainnet-beta.solana.com'
-				}, {
-					url: 'https://empty-damp-smoke.solana-mainnet.quiknode.pro/efd7051f8f55e9678d2a95e536c32e41897d8b54'
+					url: 'https://api.devnet.solana.com'	// testnet
+				}, 
+				{
+					url: 'https://api.mainnet-beta.solana.com'	// mainnet
+				}, 
+				{
+					url: 'https://api.metaplex.solana.com'	// other public url of mainnet
+				}, 
+				// {
+				// 	url: 'https://solana-devnet.g.alchemy.com/v2/iq-vSBJDLt7-8Gphy_NqUHShzguFb5Kz'	// devnet url from Alchemy
+				// },
+				{
+					url: 'https://solana-mainnet.g.alchemy.com/v2/j_0irTskpyfvy2WtK09VvhamD6IQWQjO' // mainnet url from Alchemy
+				},
+				{
+					url: 'https://empty-damp-smoke.solana-mainnet.quiknode.pro/efd7051f8f55e9678d2a95e536c32e41897d8b54'	// quicknode - fail
 				}
 			]
 		}
@@ -28,22 +41,28 @@ export default {
 			this.endpointUrl = url;
 			localStorage.setItem('EndPointUrl', this.endpointUrl);
 		},
-		getRecentBlockhash() {
+		async getRecentBlockhash() {
 			// Add web3
+			const connection = new web3.Connection(this.endpointUrl);
 
-			this.IsRequested = true;
-			this.recentBlockhash = 'GetRecentBlockhash Function was called.';
+			// console.log(connection);
 
-			localStorage.setItem('RecentBlockhash', this.recentBlockhash);
+			connection.getRecentBlockhash().then((recentBlockhash) => {
+				console.log(recentBlockhash);
+
+				this.IsRequested = true;
+				this.recentBlockhash = recentBlockhash.blockhash;
+				localStorage.setItem('RecentBlockhash', this.recentBlockhash);
+			});
 		}
-	}
+	},
 };
 </script>
 
 <template>
 	<div class="w-full">
 		<div class="leading-loose p-7 bg-secondary-light dark:bg-secondary-dark rounded-xl shadow-xl text-left">
-			<FormInput label="Solana Endpoint URL" inputIdentifier="URL" :val="endpointUrl" readonly/>
+			<FormInput label="Solana Endpoint URL" inputIdentifier="URL" :val="endpointUrl" readonly />
 			<div class="text-white">
 				No trailing slash "/". eg: http://localhost:8899
 			</div>
@@ -52,20 +71,21 @@ export default {
 			</div>
 			<div>
 				<ul>
-					<li class="text-[#a0a0a0] cursor-pointer hover:text-[#8060c0]" v-for="node in nodes" :key="node.url" @click="setNode(node.url)">
+					<li class="text-[#a0a0a0] cursor-pointer hover:text-[#8060c0]" v-for="node in nodes" :key="node.url"
+						@click="setNode(node.url)">
 						{{ node.url }}
 					</li>
 				</ul>
 			</div>
 			<div class="text-white my-4">
-				Explorer: <a class="text-[#009080] hover:text-[#8060c0]" href="https://solscan.io/" target="_blank">https://solscan.io/</a>
+				Explorer: <a class="text-[#009080] hover:text-[#8060c0]" href="https://solscan.io/"
+					target="_blank">https://solscan.io/</a>
 			</div>
 
 			<div class="flex justify-center">
 				<Button title="Get Recent Blockhash"
 					class="px-4 py-2.5 text-white tracking-wider bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 rounded-lg duration-500"
-					type="button" aria-label="Get Recent Blockhash"
-					@click="getRecentBlockhash" />
+					type="button" aria-label="Get Recent Blockhash" @click="getRecentBlockhash" />
 			</div>
 			<div class="text-white">
 				Get Recent Blockhash result:
