f9b1a9fab161d998dfb3e4ec7275ddc506add7a1
ilesoviy <ilisovij861@gmail.com>
2023-08-09 16:28:21 +0900

    Router problem has been fixed

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 import './assets/css/app.css';
 
-createApp(App)
-	.use(router)
-	.mount('#app');
+createApp(App).use(router).mount('#app')
+
 
 // Check what is the active theme and change theme when user clicks on the theme button in header.
 document.querySelector('body').classList.add('bg-primary-dark');
